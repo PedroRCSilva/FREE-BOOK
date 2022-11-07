afa99743294da1ab2dc074c96ea6140c157f5865
PedroRCSilva <pedro.rsilva@sptech.school>
2022-11-07 16:47:12 -0300

    ADD:ADICIONANDO LÓGICA JAVA SCRIPT (INCOMPLETO)

--- js/script.js
+++ js/script.js
@@ -6,11 +6,38 @@ const btn= document.querySelectorAll(".btn");
 const contentText= document.querySelector(".text");
 const div=document.querySelector(".principal .container")
 const img = document.querySelector(".content-img");
+const carrossel = document.querySelector(".session-carrossel div");
+const groupWidth = document.querySelector(".group");
+const btnCarrossel=document.querySelectorAll(".session-carrossel .btn");
+let validacao=true;
+var intervalCarrossel=setInterval(proximoCarrossel,3000);
 
-btn[0].addEventListener("click",()=>{
+
+for (key of btn){
+key.addEventListener("click",()=>{
   boll.classList.toggle("active");
 })
+}
 
-btn[1].addEventListener("click",()=>{
-  boll.classList.toggle("active");
-})
+
+
+
+function proximoCarrossel(){
+  if(carrossel.scrollWidth>carrossel.scrollLeft+1349&&validacao){
+    carrossel.scrollLeft+=carrossel.clientWidth+20;
+  }
+}
+
+function anteriorCarrossel(){
+    carrossel.scrollLeft-=carrossel.clientWidth+20;
+    if(carrossel.scrollLeft==0){
+      validacao=true;
+    }
+}
+
+
+btnCarrossel[0].addEventListener("click",anteriorCarrossel);
+btnCarrossel[1].addEventListener("click",proximoCarrossel);
+
+
+
